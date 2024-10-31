851dc8c38d32303bb17af611bbff7fbf5af54d06
skwwnlpro <skwwnlpro@gmail.com>
2024-10-31 16:04:31 +0900

    feat: add favorite functions

--- src/app/v1/favorite/repository/favorite_repository.py
+++ src/app/v1/favorite/repository/favorite_repository.py
@@ -1,5 +1,5 @@
 from src.app.v1.favorite.entity.favorite import Favorite
-from tortoise.exceptions import DoesNotExist
+from typing import List
 
 
 class FavoriteRepository:
@@ -8,18 +8,9 @@ class FavoriteRepository:
         return await Favorite.filter(user_id=user_id, movie_id=movie_id).first()
 
     @staticmethod
-    async def get_user_favorites(user_id: int) -> list[Favorite]:
+    async def get_user_favorites(user_id: int) -> List[Favorite]:
         return await Favorite.filter(user_id=user_id).all()
 
     @staticmethod
     async def add_favorite(user_id: int, movie_id: int) -> Favorite | None:
         return await Favorite.create(user_id=user_id, movie_id=movie_id)
-
-    @staticmethod
-    async def delete_favorite(user_id: int, movie_id: int) -> bool:
-        try:
-            favorite = await Favorite.filter(user_id=user_id, movie_id=movie_id).get()
-            await favorite.delete()
-            return True
-        except DoesNotExist:
-            return False
