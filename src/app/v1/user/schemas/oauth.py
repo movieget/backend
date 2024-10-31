13325ad1f01792d51b7f16ee6882022f3c738814
woojin-an <dkswoojin@gmail.com>
2024-10-31 19:27:39 +0900

    <Feat>: kakao login - add blank image_url (optional)

--- src/app/v1/user/schemas/oauth.py
+++ src/app/v1/user/schemas/oauth.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class KakaoOauthResponse(BaseModel):
     access_token: str
     id: int
-    profile_image_url: str
+    profile_image_url: Optional[str] = None
 
 
 class KakaoOauthErrorResponse(BaseModel):
