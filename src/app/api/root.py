cc8c705ebe3655d279e70448032949121ec1a0bc
skwwnlpro <skwwnlpro@gmail.com>
2024-11-01 01:14:52 +0900

    feat: add Favorite Get Check

--- src/app/api/root.py
+++ src/app/api/root.py
@@ -17,5 +17,5 @@ api_router.include_router(book.router, prefix="/books", tags=["Books"])
 api_router.include_router(user_kakao.router, prefix="/user", tags=["User_kakao"]),
 api_router.include_router(favorite.router, tags=["Favorites"])
 api_router.include_router(movie.router, prefix="/movie", tags=["Movie"])
-api_router.include_router(review.router, tages=["Reviews"])
+# api_router.include_router(review.router, tages=["Reviews"])
 api_router.include_router(payment.router, tags=["Payments"])
